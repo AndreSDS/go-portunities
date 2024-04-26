d83d17f16c4bab9152b51eaa0620d9c8f8218c98
andresds <andre.souza@asapcred.com.br>
2024-04-26 15:24:54 -0300

    Atualizar manipulador de atualização de vaga

--- handler/updateOpenning.go
+++ handler/updateOpenning.go
@@ -1,5 +1,44 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
 
-func UpdateOpeningHandler(c *gin.Context) {}
+	models "github.com/andresds/go-portunities/handler/models"
+	utils "github.com/andresds/go-portunities/handler/utils"
+	"github.com/andresds/go-portunities/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func UpdateOpeningHandler(ctx *gin.Context) {
+	id, err := utils.GetIdFromParam(ctx)
+	if err != nil {
+		return
+	}
+
+	openning := schemas.Openning{}
+	if err := db.First(&openning, id).Error; err != nil {
+		utils.SendError(ctx, http.StatusNotFound, fmt.Sprintf("openning with id: %s not found", id))
+		return
+	}
+
+	request := models.OpenningRequest{}
+	if err := ctx.BindJSON(&request); err != nil {
+		utils.SendError(ctx, http.StatusBadRequest, fmt.Sprintf("malformad request body: %s", err.Error()))
+		return
+	}
+
+	if err := utils.Validate(&request); err != nil {
+		utils.SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// cast the request to a iterable value
+	// to update the openning
+	if err := db.Model(&openning).Updates(request).Error; err != nil {
+		utils.SendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error updating opoening with id: %s", id))
+		return
+	}
+
+	utils.SendSuccess(ctx, "updating-opning", openning)
+}
