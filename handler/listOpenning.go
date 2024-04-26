a72c0ece4017b8d3a8d4f1ec98b42f37c1931e09
andresds <andre.souza@asapcred.com.br>
2024-04-26 15:44:17 -0300

    Atualizar manipulador de listagem de vagas

--- handler/listOpenning.go
+++ handler/listOpenning.go
@@ -1,5 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func ListOpeningsHandler(c *gin.Context) {}
+	models "github.com/andresds/go-portunities/handler/models"
+	utils "github.com/andresds/go-portunities/handler/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningsHandler(c *gin.Context) {
+	opennings := []models.OpenningResponse{}
+
+	if err := db.Table("opennings").Find(&opennings).Error; err != nil {
+		utils.SendError(c, http.StatusInternalServerError, "error fetching opennings")
+		return
+	}
+
+	utils.SendSuccess(c, "listopennings", opennings)
+}
